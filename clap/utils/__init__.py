22d73dfcc4825e7414e33b2bec5b84f9f8a902b1
Leon Alexander Akkad <leon@akkad.at>
2024-09-17 20:35:03 +0200

    Fixed a bug in __init__

--- clap/utils/__init__.py
+++ clap/utils/__init__.py
@@ -1,3 +1,3 @@
-from .config import create_clap_config, load_clap_config
+from .config import load_clap_config
 from .checkpoint import load_clap_ckpt, load_clf_ckpt
 from .utils import load_audio, get_target_device, set_random_seed
